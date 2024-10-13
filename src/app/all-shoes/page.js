3b57d0da17bd462892d0cb24b4c0b3f4c59b7a4e
shahin2525 <shahin451999@gmail.com>
2024-10-13 12:02:04 -0700

    add ssg

--- src/app/all-shoes/page.js
+++ src/app/all-shoes/page.js
@@ -1,16 +1,14 @@
-import Link from "next/link";
-import React from "react";
-
-const Home = async () => {
+const AlShoes = async () => {
   const res = await fetch("http://localhost:5000/shoes", {
-    next: { revalidate: 5 },
+    cache: "no-store",
   });
   const shoes = await res.json();
+
   return (
     <div>
-      <h1>next js home page</h1>
+      <h1>all shoes</h1>
       <div className="grid grid-cols-3 gap-3 p-5">
-        {shoes.slice(0, 3).map((shoe) => (
+        {shoes.map((shoe) => (
           <div key={shoe.id} className="card bg-base-100 w-80 shadow-xl">
             <figure>
               <img
@@ -25,19 +23,15 @@ const Home = async () => {
               </h2>
               <p>{shoe.description}</p>
               <div className="card-actions justify-end">
-                <div className="btn btn-secondary">Fashion</div>
-                <div className="btn btn-primary">Products</div>
+                <div className="badge badge-outline">Fashion</div>
+                <div className="badge badge-outline">Products</div>
               </div>
             </div>
           </div>
         ))}
       </div>
-      <Link href="/all-shoes">
-        {" "}
-        <div className="btn btn-primary">see all</div>
-      </Link>
     </div>
   );
 };
 
-export default Home;
+export default AlShoes;
